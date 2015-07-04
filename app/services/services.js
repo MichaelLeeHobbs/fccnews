dbea68265ab3f94a58add47cdc6dce4282e6ebd7
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-07-05 08:27:44 +0900

    added weather service

--- app/services/services.js
+++ app/services/services.js
@@ -15,4 +15,32 @@ angular.module('myApp.services', [])
             }
         };
         return sdo;
+    }]).factory('weatherSvc', ['$http', function ($http) {
+        'use strict';
+        function rangeLimit(value, min, max) {
+            value = (value >= min) ? value : min;
+            value = (value <= max) ? value : max;
+            return value;
+        }
+
+        var sdo = {
+            query: function (lat, lon, refresh) {
+                // validations
+                refresh = refresh || false;
+                // valid lat = -90...90
+                // valid lon = -180...180
+                lat = rangeLimit(lat, -90, 90);
+                lon = rangeLimit(lon, -180, 180);
+
+                var promise = $http({
+                    method: 'GET',
+                    url: 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + lat + '&lon=' + lon + '&mode=json'
+                });
+                promise.success(function (data, status, headers, conf) {
+                    return data;
+                });
+                return promise;
+            }
+        };
+        return sdo;
     }]);
