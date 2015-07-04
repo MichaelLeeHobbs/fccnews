18dca7256df71d5742e8365b069d612bb803b6b4
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-07-04 11:19:50 +0900

    Added newsSvc

--- app/news/news.js
+++ app/news/news.js
@@ -5,12 +5,19 @@ angular.module('myApp.news', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/news', {
             templateUrl: 'news/news.html',
-            controller: 'NewsCtrl'
+            controller: 'NewsCtrl',
+            resolve: {
+                newsData: ['newsSvc', function (newsSvc) {
+                    return newsSvc.get();
+                }]
+            }
         });
     }])
 
-    .controller('NewsCtrl', [function () {
+    .controller('NewsCtrl', ['$scope', 'newsData', function ($scope, newsData) {
         $(".menu-item").removeClass("active");
         $("#news").addClass("active");
+        $scope.newsData = newsData.data;
+        console.log($scope.newsData);
 
     }]);
