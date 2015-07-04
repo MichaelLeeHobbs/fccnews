83e53f9be292e73c4ba41b06552984f4c29bacb7
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-07-04 12:45:41 +0900

    updated news ctrler to get/format news data

--- app/news/news.js
+++ app/news/news.js
@@ -17,7 +17,39 @@ angular.module('myApp.news', ['ngRoute'])
     .controller('NewsCtrl', ['$scope', 'newsData', function ($scope, newsData) {
         $(".menu-item").removeClass("active");
         $("#news").addClass("active");
-        $scope.newsData = newsData.data;
-        console.log($scope.newsData);
+
+        var data = newsData.data;
+
+        // sort the stories by date
+        data.sort(function (a, b) {
+            return b.timePosted - a.timePosted;
+        });
+        $scope.newsData = data;
+
+        // get newest story
+        $scope.newestNews = data[0];
+
+        /* get trending stories */
+        var trendingNews = [];
+        $scope.trendingNews = [];
+        data.forEach(function (item) {
+            var currentDate = new Date();
+            var itemDate = new Date(item.timePosted);
+            item.score = item.rank - (currentDate.getDay() - itemDate.getDay());
+            // story must have rank > 1 to be trending, i.e. someone upvoted atleast once
+            if (item.score > 1) {
+                trendingNews.push(item);
+            }
+        });
+        // sort by score
+        trendingNews.sort(function (a, b) {
+            return b.score - a.score;
+        });
+        // get top 5 as result with hottest on top
+        for (var i = 0; i < 5; i++){
+            $scope.trendingNews.push(trendingNews[i]);
+        }
+        /* trending stories end */
 
     }]);
+
