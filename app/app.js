97bc2e1e6c31e2971d399bdd815a2d3b064d1aff
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-07-05 10:27:18 +0900

    added directive streamElement to support twitch view

--- app/app.js
+++ app/app.js
@@ -2,16 +2,27 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.services',
-  'myApp.news',
-  'myApp.weather',
-  'myApp.twitch',
-  'myApp.chat',
-  'myApp.blog',
-  'myApp.twitter',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/news'});
-}]);
+    'ngRoute',
+    'ngResource',
+    'myApp.services',
+    'myApp.news',
+    'myApp.weather',
+    'myApp.twitch',
+    'myApp.chat',
+    'myApp.blog',
+    'myApp.twitter',
+    'myApp.version'
+])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/news'});
+    }])
+    .directive('streamElement', function () {
+        return {
+            scope: {
+                data: '='
+            },
+            restrict: 'E',
+            replace: 'true',
+            templateUrl: 'directives/stream/stream-directive.html'
+        };
+    });
