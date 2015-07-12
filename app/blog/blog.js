f4873cd775c773cceb55c42a9095c4590dfeee10
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-07-12 10:30:14 +0900

    completed blog view using iframe

--- app/blog/blog.js
+++ app/blog/blog.js
@@ -2,14 +2,23 @@
 
 angular.module('myApp.blog', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/blog', {
-    templateUrl: 'blog/blog.html',
-    controller: 'BlogCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/blog', {
+            templateUrl: 'blog/blog.html',
+            controller: 'BlogCtrl'
+        });
+    }])
 
-.controller('BlogCtrl', [function() {
-      $(".menu-item").removeClass("active");
-      $("#blog").addClass("active");
-}]);
+    .controller('BlogCtrl', [function () {
+        $(".menu-item").removeClass("active");
+        $("#blog").addClass("active");
+        $("#pb-container").hide();
+
+        var blogFrameSizing = function () {
+            $("#blog-frame").height($(window).height() - $("main-menu").height() - 50);
+        };
+        blogFrameSizing();
+        $(window).resize(function () {
+            blogFrameSizing();
+        });
+    }]);
