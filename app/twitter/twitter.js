242090d1818440a0f3eda04b99dd958b8ea024ee
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-07-12 12:41:47 +0900

    This is dead code that is being keep in the event I return to this project. For now the twitter view just redirect to twitter.

--- app/twitter/twitter.js
+++ app/twitter/twitter.js
@@ -2,14 +2,23 @@
 
 angular.module('myApp.twitter', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/twitter', {
-    templateUrl: 'twitter/twitter.html',
-    controller: 'TwitterCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/twitter', {
+            templateUrl: 'twitter/twitter.html',
+            controller: 'TwitterCtrl'
+        });
+    }])
 
-.controller('TwitterCtrl', [function() {
-      $(".menu-item").removeClass("active");
-      $("#twitter").addClass("active");
-}]);
+    .controller('TwitterCtrl', [function () {
+        $(".menu-item").removeClass("active");
+        $("#twitter").addClass("active");
+        $("#pb-container").hide();
+
+        var blogFrameSizing = function () {
+            $("#twitter-frame").height($(window).height() - $("main-menu").height() - 50);
+        };
+        blogFrameSizing();
+        $(window).resize(function () {
+            blogFrameSizing();
+        });
+    }]);
