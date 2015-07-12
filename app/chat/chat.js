7e43a8f50cdb2889049e6c2fbbbf9f9e8cb1e557
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-07-12 10:29:26 +0900

    completed chat view using iframe

--- app/chat/chat.js
+++ app/chat/chat.js
@@ -2,14 +2,23 @@
 
 angular.module('myApp.chat', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/chat', {
-    templateUrl: 'chat/chat.html',
-    controller: 'ChatCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/chat', {
+            templateUrl: 'chat/chat.html',
+            controller: 'ChatCtrl'
+        });
+    }])
 
-.controller('ChatCtrl', [function() {
-      $(".menu-item").removeClass("active");
-      $("#chat").addClass("active");
-}]);
+    .controller('ChatCtrl', [function () {
+        $(".menu-item").removeClass("active");
+        $("#chat").addClass("active");
+        $("#pb-container").hide();
+
+        var chatFrameSizing = function () {
+            $("#chat-frame").height($(window).height() - $("main-menu").height() - 50);
+        };
+        chatFrameSizing();
+        $(window).resize(function(){
+            chatFrameSizing();
+        });
+    }]);
